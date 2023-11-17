b7292bdf6668c1fae3242b919e1c1e3f5d8157fa
mayank5654 <mayankkaushal99@gmail.com>
2023-11-17 11:13:44 +0530

    intial commit

---
+++ assignmet14/LargeDataCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class LargeDataCollection : IDisposable
+{
+    private int[] data; // Internal array to hold data
+
+    // Property to get the length of the collection
+
+    public int Length
+    {
+        get { return data.Length; }
+    }
+
+    // Constructor to initialize the collection with initial data
+    public LargeDataCollection(int[] initialData)
+    {
+        
+        data = initialData;
+    }
+
+
+    // Implement logic to add an element to the collection
+    // For demonstration purposes, appending the element to the end
+    public void AddElement(int element)
+    {
+        Array.Resize(ref data, data.Length + 1);
+        data[data.Length - 1] = element;
+    }
+
+    // Implement logic to remove an element from the collection
+    // For demonstration purposes, removing the first occurrence of the element
+    public void RemoveElement(int element)
+    {
+        
+        int index = Array.IndexOf(data, element);
+        if (index != -1)
+        {
+            for (int i = index; i < data.Length - 1; i++)
+            {
+                data[i] = data[i + 1];
+            }
+            Array.Resize(ref data, data.Length - 1);
+        }
+    }
+
+    // Implement logic to access an element at a specific index
+    // For demonstration purposes, returning the element at the specified index
+    public int GetElement(int index)
+    {
+        
+        return data[index];
+    }
+
+    // Implement Dispose method to release resources and free up memory
+    // Set the internal data structure to null
+    public void Dispose()
+    {
+       
+        data = null;
+    }
+}
---
+++ assignmet14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmet14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create an instance of LargeDataCollection
+            int[] initialData = { 1, 2, 3, 4, 5 };
+            using (LargeDataCollection dataCollection = new LargeDataCollection(initialData))
+            {
+                // Demonstrate adding, removing, and accessing elements
+                dataCollection.AddElement(9);
+                dataCollection.RemoveElement(1);
+                int elementAtIndex = dataCollection.GetElement(4);
+
+                // Display the modified collection
+                Console.WriteLine("Modified Collection:");
+                for (int i = 0; i < dataCollection.Length; i++)
+                {
+                    Console.Write(dataCollection.GetElement(i) + " ");
+                }
+            } // Dispose will be called automatically when exiting the 'using' block
+
+        }
+    }
+    
+
+
+
+}
+
